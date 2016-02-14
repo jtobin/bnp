4136c0236c0cba3a4bf73134602decc2d30fbebb
Jared Tobin <jared@jtobin.ca>
2016-02-14 16:42:07 +1300

    Get model working.

--- finite-gaussian-mixture/src/fmm_multivariate_generative.r
+++ finite-gaussian-mixture/src/fmm_multivariate_generative.r
@@ -10,28 +10,22 @@ precision_model = function(k, b, w) rinvwishart(k, b, solve(w))
 
 parameter_model = function(m, k, n) {
   p  = mixing_model(k, 1)
-
   c  = delabel(lapply(label_model(n, p), list))
-
   mu = delabel(apply(location_model(k, rep(0, m), diag(10, m)), MARGIN = 1, list))
-
   s  = precision_model(k, 10, diag(1, m))
-
   list(c, mu, s)
   }
 
-# FIXME mapply not working here
-# want to get mapply to work on these weird structures i've cobbled together
-# mapply will work an delabelled lists
 data_model = function(config) {
-  sampler = function(c, m, s) rmvnorm(c, m, solve(s))
-  mapply(sampler, config[[1]], config[[2]], config[[3]])
+  mapply(safe_rmvnorm, config[[1]], config[[2]], config[[3]])
   }
 
-model = function(k, n) {
-  config = parameter_model(k, n)
+model = function(m, k, n) {
+  config = parameter_model(m, k, n)
   data_model(config)
   }
+
+# utilities
 
 rinvwishart = function(n, v, S) {
   wishes = rWishart(n, v, solve(S))
@@ -39,3 +33,9 @@ rinvwishart = function(n, v, S) {
   }
 
 delabel = function(x) { lapply(x, "[[", 1) }
+
+safe_rmvnorm = function(c, m, s) {
+  if (c <= 0) return(numeric(0))
+  else rmvnorm(c, m, solve(s))
+  }
+
